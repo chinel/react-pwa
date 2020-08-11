32f6876cd9db5aebad30fa7070f93ba0772a87ec
nelo <nelokwaz@gmail.com>
2020-08-11 10:32:11 +0100

    pulled video data from an api and displayed them in the app component file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect}from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+      const fetchData = async () => {
+          const result = await  fetch("http://orangevalleycaa.org/api/videos").then(
+              response => response.json()
+          );
+         setData(result);
+      }
+      fetchData();
+  },[]);
   return (
     <div className="App">
       <header>
        <h1>Videos</h1>
       </header>
+        {
+            data.map(video => (
+                <div key={video.id}>
+                    <h2>{video.name}</h2>
+                    <video height={200} width={200} controls src={video.video_url}/>
+                </div>
+            ))}
     </div>
   );
 }
